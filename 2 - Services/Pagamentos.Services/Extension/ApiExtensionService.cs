2b22f775069f0ab6923dfccc67b298090fbaefe5
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-11-29 22:54:22 -0300

    feat - Adiciona APIExtension para centralização do Program

---
+++ 2 - Services/Pagamentos.Services/Extension/ApiExtensionService.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Pagamentos.Service.Extension
+{
+    public static class ApiExtensionService
+    {
+
+        public static void WebApplicationBuilderExtension(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+        }
+    }
+}
